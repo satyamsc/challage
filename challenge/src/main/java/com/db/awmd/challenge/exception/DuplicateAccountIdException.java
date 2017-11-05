043119825b7c06434bf53292b51857c049351b0d
satyamsc <33126196+satyamsc@users.noreply.github.com>
2017-11-05 21:07:20 +0100

    commit
    co

--- challenge/src/main/java/com/db/awmd/challenge/exception/DuplicateAccountIdException.java
+++
@@ -1,8 +0,0 @@
-package com.db.awmd.challenge.exception;
-
-public class DuplicateAccountIdException extends RuntimeException {
-
-  public DuplicateAccountIdException(String message) {
-    super(message);
-  }
-}
